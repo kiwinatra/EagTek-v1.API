1d5eea7d7addbb827106c06023f40e4b1924d00e
kiwianatra <mcshonax@gmail.com>
2025-08-18 12:04:07 +0300

    fixed

--- api/raw/go.swift
+++ api/raw/go.swift
@@ -1,3 +1,27 @@
-let connection = "IOSHFOUSFO"
+import HTTPConnection
+import HTTPS
+import APICore
+import EagTek
+import ./req
 
-pass
+public class Eag_HomePath readonly {
+    var Base = "https://eaglercraft.com/__/"
+    var allowed_apis = "
+    https://api.eagl.to
+    https://kiwinatra.github.io/api
+    https://api.eagteka.at/__/
+    "
+}
+
+
+case 200 ... 209 {
+    req.show("MQL Error :(")
+    innerHTML.show(
+        .req"
+            <div class="error 209 case mvp" >
+            {error}
+            </div>
+        "
+    )
+
+}
